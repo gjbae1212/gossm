366faca8f0548e923c54ad0f9388360c3206e4e8
gjbae1212 <gjbae1212@gmail.com>
2019-07-14 21:11:01 +0900

    Added temporary code.

--- cmd/start_session.go
+++ cmd/start_session.go
@@ -1,8 +1,14 @@
 package cmd
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ssm"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -26,6 +32,25 @@ var (
 			}
 		},
 		Run: func(cmd *cobra.Command, args []string) {
+			svc := ssm.New(awsSession, aws.NewConfig().WithRegion(viper.GetString("region")))
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+			defer cancel()
+			inst := viper.GetString("instance")
+			sess, err := svc.StartSessionWithContext(ctx, &ssm.StartSessionInput{Target: &inst})
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			fmt.Println(*sess.SessionId)
+			fmt.Println(*sess.StreamUrl)
+			fmt.Println(*sess.TokenValue)
+			a, err := json.Marshal(sess)
+			fmt.Println(string(a))
+			b, err := json.Marshal(&ssm.StartSessionInput{Target: &inst})
+			fmt.Println(string(b))
+			fmt.Println(svc.Endpoint)
+			// TODO: session-manager-plugin 설치
+			// TODO: session-manager-plugin 'json' region StartSession
 		},
 	}
 )
