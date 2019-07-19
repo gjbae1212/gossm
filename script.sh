35e30205fe1c579021cb85c578c5861c869e1a9e
gjbae1212 <gjbae1212@gmail.com>
2019-07-20 00:59:55 +0900

    Added goreleaser.

--- script.sh
+++ script.sh
@@ -9,6 +9,16 @@ function test
     go test -v $(go list ./... | grep -v vendor) --count 1 -race -coverprofile=$CURRENT/coverage.txt -covermode=atomic
 }
 
+function build
+{
+   packr2 && go build
+}
+
+function release
+{
+   goreleaser release --rm-dist
+}
+
 CMD=$1
 shift
 $CMD $*
